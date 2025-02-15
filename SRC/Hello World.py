e30e4f54c9061dc06316c1e637f39578905852e1
Michael Henrique <michaelhenriquepedro@gmail.com>
2025-02-15 00:10:40 -0300

    teste loops

--- SRC/Hello World.py
+++ SRC/Hello World.py
@@ -1,1 +1,10 @@
 print ("Olá, Mundo!")
+contador = 0
+while contador < 10:
+    print (contador)
+    contador = contador + 1
+print ("Fim do programa")
+
+frutas = ["maçã", "banana", "laranja"]
+for fruta in frutas:
+    print (fruta)
